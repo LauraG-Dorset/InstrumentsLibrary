ee4f7fde171ea4dcfccca4d879d31f6377c506ac
Laura <Laura@DESKTOP-POTM4KB>
2021-11-10 16:40:23 +0000

    Ajoutez des fichiers projet.

---
+++ InstrumentsLibrary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentsLibrary
+{
+    public abstract class Instruments
+    {
+        public string serialNumber { get; set; }
+        public string Name { get; set; }
+        public Instruments(string _name)
+        {
+            Name = _name;
+        }
+        public abstract void Play();
+        public class guitar : Instruments
+        {
+            public guitar(string _name):base(_name)
+            {
+
+            }
+
+            public override void Play()
+            {
+                //throw new NotImplementedException();
+                Console.WriteLine($"I am playing {Name} guitar");
+            }
+        }
+        public class mandolin : Instruments
+        {
+            public mandolin(string _name) : base(_name)
+            {
+
+            }
+
+            public override void Play()
+            {
+                Console.WriteLine($"I am playing {Name} mandolin");
+            }
+        }
+        public class ukulele: Instruments
+        {
+            public ukulele(string _name) : base(_name)
+            {
+
+            }
+
+            public override void Play()
+            {
+                Console.WriteLine($"I am playing {Name} mandolin");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InstrumentsLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            guitar myguitar = new guitar("Gibson");
+            mandolin Kentucky = new mandolin("Kentucky");
+            Ukulele myUkele = new Ukulele("ortega");
+
+            myUkulele.serialNumber = "abc123";
+
+            myguitar.Play();
+            Kentucky.Play();
+            myUkele.Play();
+        }
+    }
+}
